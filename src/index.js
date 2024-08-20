7dffabd5b28f82a93b5a25aaaec093722aaea172
saroj9 <saroj@distronix.in>
2024-08-20 13:58:03 +0530

    some configuration added in app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Err:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is running on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("mongodb connection failed!!", err));
